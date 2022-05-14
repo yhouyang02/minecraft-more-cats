4afce5d45aef654c70ef65b4be9de82a99092fe9
yhouyang02 <yhouyang02@outlook.com>
2022-05-14 14:10:53 +0800

    Initial commit

---
+++ src/main/java/site/yhouyang/mcmorecats/MoreCats.java
@@ -0,0 +1,29 @@
+package site.yhouyang.mcmorecats;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreCats implements ModInitializer {
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    public static final Item TEST_CAT_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+        Registry.register(Registry.ITEM, new Identifier("mcmorecats", "test_cat_ball"),
+                TEST_CAT_BALL);
+    }
+}
