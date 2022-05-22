b1d6fec7fed79e1c892c59b4114dd5b6346fb973
yhouyang02 <yhouyang02@outlook.com>
2022-05-22 19:42:55 +0800

    Add test block
    1. Added test cat block with texture
    2. TODO: Add drops to blocks
    3. TODO: Add item textures to blocks

--- src/main/java/site/yhouyang/mcmorecats/MoreCats.java
+++ src/main/java/site/yhouyang/mcmorecats/MoreCats.java
@@ -1,8 +1,13 @@
 package site.yhouyang.mcmorecats;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
@@ -15,6 +20,8 @@ public class MoreCats implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
     public static final Item TEST_CAT_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Block TEST_CAT_BLOCK = new Block(
+            FabricBlockSettings.of(Material.STONE).strength(1.8F, 1.8F).sounds(BlockSoundGroup.STONE).requiresTool());
 
     @Override
     public void onInitialize() {
@@ -23,7 +30,13 @@ public class MoreCats implements ModInitializer {
         // Proceed with mild caution.
 
         LOGGER.info("Hello Fabric world!");
-        Registry.register(Registry.ITEM, new Identifier("mcmorecats", "test_cat_ball"),
-                TEST_CAT_BALL);
+
+        // register test item
+        Registry.register(Registry.ITEM, new Identifier("mcmorecats", "test_cat_ball"), TEST_CAT_BALL);
+
+        // register test block and its corresponding item
+        Registry.register(Registry.BLOCK, new Identifier("mcmorecats", "test_cat_block"), TEST_CAT_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("mcmorecats", "test_cat_block"),
+                new BlockItem(TEST_CAT_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
